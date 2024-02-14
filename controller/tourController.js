59863f51d17269592636fd579d1454db37530357
SachyamDh <iamsaxyamdh@gmail.com>
2024-02-14 07:37:01 +0545

    Pagintion done

--- controller/tourController.js
+++ controller/tourController.js
@@ -7,10 +7,12 @@ const getAllTours = async (req, res) => {
     const excludedFields = ["page", "sort", "limit", "fiels"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
+    //filter
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     let query = Tour.find(JSON.parse(queryStr));
 
+    //sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(",").join(" ");
       console.log(sortBy);
@@ -19,12 +21,25 @@ const getAllTours = async (req, res) => {
       query = query.sort("-createdDate");
     }
 
+    //Limiting Fields
     if (req.query.fields) {
       const fields = req.query.fields.split(",").join(" ");
       query = query.select(fields);
-    }else{
-      query = query.select('-__v')
+    } else {
+      query = query.select("-__v");
     }
+
+    //Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("Page does not exist");
+    }
+
     const tours = await query;
 
     res.status(200).json({
