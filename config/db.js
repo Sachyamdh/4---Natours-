7c1c745404148566f2869a519ca7b55213e2923c
SachyamDh <iamsaxyamdh@gmail.com>
2024-02-15 06:17:09 +0545

    Data added on cloud

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
 const mongoose = require("mongoose");
+const Tour = require("../models/toursModel");
 
 const db = process.env.DATABASE.replace(
   "<password>",
@@ -13,5 +15,34 @@ const dbConnect = async () => {
     console.log("Connection to Database Failed", error);
   }
 };
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
 module.exports = { dbConnect };
--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -11,7 +11,7 @@ const {
 const Router = express.Router();
 
 // Router.param("id", checkId);
-Router.route("/top-5-cheap").get(aliasTopTours(getAllTours));
+Router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 Router.route("/tour-stats").get(tourStats);
 Router.route("/").get(getAllTours).post(createTour);
 Router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
