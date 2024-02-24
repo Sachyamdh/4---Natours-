8b8b6f8a7b80512d5ef89dd8f471cf2f4ed7000e
SachyamDh <iamsaxyamdh@gmail.com>
2024-02-24 08:50:33 +0545

    Production and deployment errors

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProduction = (err, res) => {
+  //operational, trusted error: send message. These are errors from our hard code
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.messsage,
+    });
+  }
+  //programming or other errors send minimum message. These are errors from our third party app
+  else {
+    console.error("Error", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProduction(err, res);
+  }
 };
